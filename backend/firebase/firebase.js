237e4469af79377dfa27376f51681e1142108c39
x3penguin <52276833+x3penguin@users.noreply.github.com>
2025-03-15 18:30:40 +0800

    This back and forth gotta stop

--- backend/firebase/firebase.js
+++ backend/firebase/firebase.js
@@ -9,6 +9,8 @@ dotenv.config();
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+dotenv.config();
+
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,

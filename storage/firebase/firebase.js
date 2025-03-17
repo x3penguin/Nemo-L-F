2b26270223a596d59af2bffa1d7673715a27d415
ryanhung919 <ryanhung919@gmail.com>
2025-03-17 22:53:50 +0800

    auth from firebase

--- storage/firebase/firebase.js
+++ storage/firebase/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import dotenv from 'dotenv';
@@ -23,7 +24,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, storage };
+export { app, auth, db, storage };
